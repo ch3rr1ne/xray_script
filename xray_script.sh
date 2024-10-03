08e67b827a96f254fd4d5a286ac5cd700a49ab67
ch3rr1ne <96495767+ch3rr1ne@users.noreply.github.com>
2024-10-03 16:49:45 +0800

    接近完整

--- xray_script.sh
+++ xray_script.sh
@@ -2,14 +2,6 @@
 # Write By Sone
 #https://github.com/ch3rr1ne
 
-#定义Github token
-OAUTHTOKEN='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'
-
-#定义地址
-nginx='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/nginx.conf'
-kernel='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/sysctl.conf'
-xray_config='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/config_server.json'
-
 #颜色
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
@@ -32,7 +24,9 @@ function _wrongNumber() {
   exit 0
 }
 
-#最新nginx
+
+
+#获取最新nginx
 function get_nginx() {
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list
@@ -40,26 +34,37 @@ echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 
 }
 
 #nginx配置
-function _nginx() {
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /etc/nginx/nginx.conf "$nginx"
+function _nginx_conf() {
+    curl -H "Authorization: Bearer "${token}"" -Lo /etc/nginx/nginx.conf "$nginx"
 }
 
 #内核优化
 function _kernel() {
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /etc/sysctl.conf "$kernel"
+    curl -H "Authorization: Bearer "${token}"" -Lo /etc/sysctl.conf "$kernel"
     sysctl -p
 }
 
 #xray 配置
 function _xray_config() {
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /usr/local/etc/xray/config.json "$xray_config"
+    curl -H "Authorization: Bearer "${token}"" -Lo /usr/local/etc/xray/config.json "$xray_config"
 }
 #xrayr配置
 function _xrayr_config(){
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /usr/local/etc/xray/config.json "$xrayr_config"
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /etc/XrayR/cert/nanodesu.key "$key"
-    curl -H "Authorization: Bearer "${OAUTHTOKEN}"" -Lo /etc/XrayR/cert/Certificate.crt "$cer"
+    curl -H "Authorization: Bearer "${token}"" -Lo /etc/XrayR/cert/nanodesu.key "$key"
+    curl -H "Authorization: Bearer "${token}"" -Lo /etc/XrayR/cert/Certificate.crt "$cer"
+white " 1. VISION-TLS"
+white " 2. VISION-REALITY"
+read -p "请选择: [默认:VISION-TLS]" conf
+[ -z "${conf}" ] && conf="1"
+	if [[ $conf == [1] ]]; then
+		curl -H "Authorization: Bearer "${token}"" -Lo etc/XrayR/config.json "$xrayr_config"
+	elif  [[ "$conf" == "2" ]]; then
+        curl -H "Authorization: Bearer "${token}"" -Lo /etc/XrayR/config.yml "$reality_config"
+    else 
+echo "吗的,选1或者2啊"
+    fi   
 }
+
 #环境
 function _os() {
   local os=""
@@ -75,7 +80,7 @@ function _install_nginx() {
       dnf update -y
       dnf install -y dnf-plugins-core
       dnf update -y
-      dnf install -y gnupg2 ca-certificates curl wget
+      dnf install -y gnupg2 sl ca-certificates curl wget
       get_nginx
       dnf install -y nginx
       for package_name in ${packages_name}; do
@@ -85,7 +90,7 @@ function _install_nginx() {
       yum update -y
       yum install -y epel-release yum-utils
       yum update -y
-      yum install -y gnupg2 ca-certificates curl wget
+      yum install -y gnupg2 sl ca-certificates curl wget
       get_nginx
       yum install -y nginx
       for package_name in ${packages_name}; do
@@ -95,7 +100,7 @@ function _install_nginx() {
     ;;
   ubuntu | debian)
     apt update -y 
-    apt install -y gnupg2 ca-certificates lsb-release debian-archive-keyring curl wget
+    apt install -y gnupg2 sl ca-certificates lsb-release debian-archive-keyring curl wget
     get_nginx
     apt install -y nginx
     for package_name in ${packages_name}; do
@@ -108,13 +113,25 @@ function _install_nginx() {
 
 #安装Xray
 function _install_xray(){
+if [[ -z "${xray_config}" ]]; then
+echo "你装错了"
+    exit 0
+fi
+if [[ -z "${xrayr_config}" ]]; then
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 systemctl restart xray && systemctl restart nginx && sleep 0.2 && systemctl status xray && systemctl status nginx
+fi
 }
 
 #安装XrayR
 function _install_xrayR(){
-bash -c "$(curl -L https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)"
+    if [[ -z "${xray_config}" ]]; then
+    bash -c "$(curl -L https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)"
+fi
+if [[ -z "${xrayr_config}" ]]; then
+echo "你装错了"
+    exit 0
+    fi
 }
 
 #接入面板
@@ -122,7 +139,7 @@ function _dashboard(){
     read -p "请输入 面板ID: " dashid
     if [[ -z "$dashid" ]]; then
         red "面板ID不能为空！"
-        return 1
+        _dashboard
     fi
 echo "你输入的面板ID为 $dashid"
 
@@ -131,7 +148,7 @@ echo "你输入的面板ID为 $dashid"
     read -p "请输入 接入域名: " domain
     if [[ -z "$domain" ]]; then
         red "域名不能为空！"
-        return 1
+        _dashboard
     fi
 echo "你输入的域名为 $domain"
     sed -i "79s/fake/${domain}/g" /etc/XrayR/config.yml
@@ -141,27 +158,18 @@ echo "接入成功!"
 function all () {
     _install_nginx
     sleep 0.5
-    clear
-    white 1. Xray
-    white 2. XrayR
-    read -p "选择安装的版本:" ray
-case "$ray" in
-    1 )
-    _install_xray
-    ;;
-    2 )
+if [[ -z "${xray_config}" ]]; then
     _install_xrayR
-    ;;
-    esac
-
-    sleep 0.5
+    _xrayr_config
+fi
+if [[ -z "${xrayr_config}" ]]; then
     _install_xray
+    _xray_config
+fi
     sleep 0.5
     _kernel
     sleep 0.5
-    _xray_config
-    sleep 0.5
-    _nginx
+    _nginx_conf
     clear
     echo "安装完成！"
     systemctl restart xray && systemctl restart nginx && sleep 0.2 && systemctl status xray && systemctl status nginx && nginx -t
@@ -179,7 +187,7 @@ function _uninstall (){
 #下载配置
 function _config(){
     # 下载 nginx 和内核优化配置
-    _nginx
+    _nginx_conf
     _kernel
 if [[ -z "${xray_config}" ]]; then
     _xrayr_config
@@ -192,15 +200,14 @@ function Sone (){
 nginx='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/nginx.conf'
 kernel='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/sysctl.conf'
 xray_config='https://raw.githubusercontent.com/ch3rr1ne/reality/refs/heads/main/config_server.json'
-OAUTHTOKEN='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'
 }
 function Endblc (){
 nginx='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/nginx.conf'
 kernel='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/sysctl.conf'
 xrayr_config='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/config.yml'
+reality_config='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/reality.yml'
 cer='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/Certificate.crt'
 key='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/nanodesu.key'
-OAUTHTOKEN='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'
 }
 
 #更改配置
@@ -213,6 +220,7 @@ exit 0
 function menu() 
 {
 clear
+read -p "请先填入你的token: " token
 blue "-----请先选择你的英雄-----"
 white " 1. Sone"
 white " 2. Endblc"
@@ -237,6 +245,7 @@ green " 4. 内核调优"
 green " 5. 下载配置"
 green " 6. 接入面板"
 green " 7. 更改配置" 
+green " 8. 介是嘛呀"
 yellow " =================================================="
 blue " 9.  我全都要安装"
 red " 00. 全给我卸载了"
@@ -264,6 +273,9 @@ read -p "请输入数字:" NumberInput
 ;;
     7 )
         _change
+;;
+    8 )
+        sl
 ;;
     9 )
         all
