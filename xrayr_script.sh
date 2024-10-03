78926fbdfcb1c31864a4f5c3eaae05735a311283
ch3rr1ne <chairmach@foxmail.com>
2024-10-04 06:52:38 +0800

    add

--- xrayr_script.sh
+++ xrayr_script.sh
@@ -127,5 +127,5 @@ cer='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/Certificate.c
 key='https://raw.githubusercontent.com/Endblc/xcfg/refs/heads/main/nanodesu.key'
 }
 
-
+[[ $EUID -ne 0 ]] white "用Root用户运行"
 all
